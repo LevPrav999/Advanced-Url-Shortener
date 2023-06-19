d9c909b60d8ee93480f458c23b8eb738fa05a342
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-06-19 16:49:09 +0300

    Initial commit

---
+++ src/main/java/ru/levprav/shortener/ShortenerApplication.java
@@ -0,0 +1,13 @@
+package ru.levprav.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerApplication.class, args);
+	}
+
+}
