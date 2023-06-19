3319b39cc354f97ad25e0386198583d02132dc2d
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-06-19 18:39:07 +0300

    add dto

---
+++ src/main/java/ru/levprav/shortener/dto/CreateLinkDto.java
@@ -0,0 +1,16 @@
+package ru.levprav.shortener.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLinkDto {
+    @Size(max = 85, message = "Url is too long!")
+    private String targetURL;
+}
