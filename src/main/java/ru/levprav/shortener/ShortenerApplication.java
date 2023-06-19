814b760a1cb9ae8cd183b67250dcc333bfddc247
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-06-19 18:40:35 +0300

    add main file

--- src/main/java/ru/levprav/shortener/ShortenerApplication.java
+++ src/main/java/ru/levprav/shortener/ShortenerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortenerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortenerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerApplication.class, args);
+    }
 
 }
