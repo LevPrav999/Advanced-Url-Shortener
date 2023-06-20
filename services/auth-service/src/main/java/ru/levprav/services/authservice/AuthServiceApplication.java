f0602e73aeaa045f80dcc4cb30d814d544e184ea
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-06-20 18:28:27 +0300

    add auth service

---
+++ services/auth-service/src/main/java/ru/levprav/services/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.levprav.services.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
